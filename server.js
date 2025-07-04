5eb5091caabf4a734dda1146fd10dc67a1c359f8
priyanka-kolhe <pkolhe657@gmail.com>
2025-07-04 10:42:36 +0530

    Get method

--- server.js
+++ server.js
@@ -9,9 +9,20 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-const a = 10;
-const b = 20;
-console.log(a + b);
+// const a = 10;
+// const b = 20;
+// console.log(a + b);
+
+app.get("/hello", () =>{
+    console.log("hello");
+})
+app.get("/", (req,res) => {
+    res.json({"message" : "Hello World...!"})
+})
+
+app.get("/health",(req,res)=>{
+    res.json({"message" : "Health is good!"})
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running is on port ${PORT}`)
